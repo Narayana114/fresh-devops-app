5b08bd6a0852d083c937b29141be77a69cc9dbc9
Narayana114 <103238724+Narayana114@users.noreply.github.com>
2026-04-24 12:35:39 +0530

    Update new love app.js

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 const http = require('http');
+const fs = require('fs');
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+
+  const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
   res.writeHead(200, { 'Content-Type': 'text/html' });
 
   res.end(`
     <html>
       <head>
-        <title>Love App</title>
+        <title>${data.title}</title>
         <style>
           body {
-            background: linear-gradient(to right, #ff758c, #ff7eb3);
-            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            font-family: Arial;
             text-align: center;
+            background: linear-gradient(to right, #ff758c, #ff7eb3);
             color: white;
-            margin-top: 100px;
+          }
+          .container {
+            margin-top: 80px;
           }
           h1 {
             font-size: 50px;
-            animation: glow 2s infinite alternate;
           }
           h2 {
             font-size: 30px;
           }
-          @keyframes glow {
-            from { text-shadow: 0 0 10px white; }
-            to { text-shadow: 0 0 30px red; }
+          img {
+            width: 200px;
+            margin-top: 20px;
+            border-radius: 20px;
           }
           .heart {
-            font-size: 60px;
+            font-size: 40px;
             animation: beat 1s infinite;
           }
           @keyframes beat {
@@ -37,14 +45,24 @@ const server = http.createServer((req, res) => {
             50% { transform: scale(1.2); }
             100% { transform: scale(1); }
           }
+          footer {
+            margin-top: 40px;
+            font-size: 18px;
+          }
         </style>
       </head>
 
       <body>
-        <h1>❤️ Love You ❤️</h1>
-        <h2>ye pichamma 💖</h2>
-        <div class="heart">💓 💓 💓</div>
-        <p>🚀 Deployed using Jenkins + Docker + Kubernetes</p>
+        <div class="container">
+          <h1>❤️ ${data.message}</h1>
+          <h2>${data.names}</h2>
+
+          <div class="heart">💓 💓 💓</div>
+
+          <img src="${data.image}" />
+
+          <footer>${data.footer}</footer>
+        </div>
       </body>
     </html>
   `);
