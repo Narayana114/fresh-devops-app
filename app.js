4760067157f7bfab386edf29209674a4701cfbd0
Narayana114 <103238724+Narayana114@users.noreply.github.com>
2026-04-24 11:42:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 const http = require('http');
 
+const PORT = 3000;
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Love you ye pichamma ❤️');
+
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`
+      <html>
+        <head>
+          <title>My Real App</title>
+        </head>
+        <body style="font-family: Arial; text-align:center; margin-top:50px;">
+          <h1>🚀 DevOps App Running</h1>
+          <h2>❤️ Love you ye pichamma ❤️</h2>
+          <p>Welcome to my real Node.js app</p>
+          <a href="/api">Go to API</a>
+        </body>
+      </html>
+    `);
+  }
+
+  else if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      message: "API working",
+      status: "success",
+      app: "real-devops-app"
+    }));
+  }
+
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page not found');
+  }
+
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
