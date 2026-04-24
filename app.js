6497e5d0b3ea15208203851d23cd15983bcd4b19
Narayana114 <103238724+Narayana114@users.noreply.github.com>
2026-04-24 12:04:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,40 +3,18 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(`
-      <html>
-        <head>
-          <title>My Real App</title>
-        </head>
-        <body style="font-family: Arial; text-align:center; margin-top:50px;">
-          <h1>🚀 DevOps App Running</h1>
-          <h2>❤️ Love you ye pichamma ❤️</h2>
-          <p>Welcome to my real Node.js app</p>
-          <a href="/api">Go to API</a>
-        </body>
-      </html>
-    `);
-  }
-
-  else if (req.url === '/api') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-      message: "API working",
-      status: "success",
-      app: "real-devops-app"
-    }));
-  }
-
-  else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Page not found');
-  }
-
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`
+    <html>
+      <body style="font-family:Arial;text-align:center;margin-top:80px;">
+        <h1>🚀 My DevOps App is Running</h1>
+        <h2>Love you ye pichamma ❤️</h2>
+        <p>Deployed using Jenkins + Docker + Kubernetes</p>
+      </body>
+    </html>
+  `);
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`App running on port ${PORT}`);
 });
